ca642475308ca293923714bdbe54e19f0c2e5171
aisalbek <110242974+aisalbek@users.noreply.github.com>
2022-09-11 17:11:37 +0600

    a

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Car {
+public class Car {
+    int id;
+    int nomermashiny;
+
+    public Car(int id, int nomermashiny) {
+        this.id = id;
+        this.nomermashiny = nomermashiny;
+    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public int getNomermashiny() {
+//        return nomermashiny;
+//    }
+//
+//    public void setNomermashiny(int nomermashiny) {
+//        this.nomermashiny = nomermashiny;
+//    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", nomermashiny=" + nomermashiny +
+                '}';
+    }
 }
--- src/Car2.java
+++ src/Car2.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class Car2 {
+public class Car2 {
+    int godvypuska;
+    String model;
+    int sena;
+    String color;
+
+    public Car2(int godvypuska, String model, int sena, String color) {
+        this.godvypuska = godvypuska;
+        this.model = model;
+        this.sena = sena;
+        this.color = color;
+    }
+//
+//    public int getGodvypuska() {
+//        return godvypuska;
+//    }
+//
+//    public void setGodvypuska(int godvypuska) {
+//        this.godvypuska = godvypuska;
+//    }
+//
+//    public String getModel() {
+//        return model;
+//    }
+//
+//    public void setModel(String model) {
+//        this.model = model;
+//    }
+//
+//    public int getSena() {
+//        return sena;
+//    }
+//
+//    public void setSena(int sena) {
+//        this.sena = sena;
+//    }
+//
+//    public String getColor() {
+//        return color;
+//    }
+//
+//    public void setColor(String color) {
+//        this.color = color;
+//    }
+
+    @Override
+    public String toString() {
+        return "Car2{" +
+                "godvypuska=" + godvypuska +
+                ", model='" + model + '\'' +
+                ", sena=" + sena +
+                ", color='" + color + '\'' +
+                '}';
+    }
 }
